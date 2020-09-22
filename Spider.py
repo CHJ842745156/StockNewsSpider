520493a322350c6ca5e249893d11e62d8223824c
CHJ842745156 <842745156@qq.com>
2020-09-22 21:55:29 +0800

    20200922
    1111

---
+++ Spider.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 20:54:57 2020
+
+@author: CHJ
+"""
+
+import requests as req
+from bs4 import BeautifulSoup
+
+url = 'https://www.toutiao.com/c/user/token/MS4wLjABAAAA56eGL40AT6T7eLJ7ir3rhX74Fa6LtzNWkfcq2jcXAek/'
+
+HtmlStr = req.get(url)
+
+soup = BeautifulSoup(HtmlStr,'lxml')
+#print(soup.title)
+#print(soup.p['class'])
+#print(soup.find_all('a'))
+#print(soup)
+print(soup.prettify())
---
+++ proxy.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 20:53:31 2020
+
+@author: CHJ
+"""
+
